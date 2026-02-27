39309b975735c7043bc31f9bb317aca5699fadbe
temporal-architect <amexsimoes@gmail.com>
2026-02-28 01:42:50 +0800

    Upgrade: Parallelized S-Matrix Propagator via OpenMP. Multi-core scaling active.

--- kissat_sovereign/src/engine.cpp
+++ kissat_sovereign/src/engine.cpp
@@ -1,7 +1,6 @@
 /* * Copyright (c) 2026 Americo Simoes
  * License: Sovereign Research License (SRL)
- * * SOVEREIGN-LOGIC: Unitary SAT Propagator
- * Mathematical Core: Reihman-Lock Hamiltonian
+ * * SOVEREIGN-LOGIC: Multi-Threaded Unitary SAT Propagator
  */
 
 #include <iostream>
@@ -9,21 +8,22 @@
 #include <cmath>
 #include <complex>
 #include <fstream>
+#include <omp.h>
 
 const float BETA = 1.23f;
 const float LAMBDA = 0.963f;
 
 extern "C" {
     void solve_manifold(const unsigned char* input, unsigned char* output, int width, int height) {
-        double phase = 0.42317;
+        #pragma omp parallel for schedule(dynamic)
         for (int y = 0; y < height; ++y) {
+            double phase = 0.42317 + (y * 0.00157 * LAMBDA);
             for (int x = 0; x < width; ++x) {
                 float raw = (float)input[y * width + x] / 255.0f;
                 float energy = std::tanh(BETA * (raw - 0.5f) * LAMBDA);
                 std::complex<double> unitary = std::exp(std::complex<double>(0, -1) * (double)energy * (33.0 / BETA));
                 output[y * width + x] = (unsigned char)(std::abs(unitary.real() + std::sin(phase)) * 255.0f);
             }
-            phase += 0.00157 * LAMBDA;
         }
     }
 }
@@ -39,9 +39,15 @@ int main(int argc, char** argv) {
     std::vector<unsigned char> input(w * h);
     std::vector<unsigned char> output(w * h);
     in.read((char*)input.data(), w * h);
+    
+    double start = omp_get_wtime();
     solve_manifold(input.data(), output.data(), w, h);
+    double end = omp_get_wtime();
+    
     std::ofstream out(argv[2], std::ios::binary);
     out << "P5\n" << w << " " << h << "\n255\n";
     out.write((char*)output.data(), w * h);
+    
+    std::cout << "Propagation Time: " << (end - start) << "s" << std::endl;
     return 0;
 }
